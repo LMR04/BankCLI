58ec111a1a54871e9b6e04e07ecd20a874147641
Luis Rubio <102443174+LMR04@users.noreply.github.com>
2026-04-21 10:45:16 -0500

    feat: show user information

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
 
+const int MAX_USERS = 100;
+
 struct User {
     int id;
+    int pin;
     std::string name;
     double balance;
 };
 
 int main() {
-    std::cout << "Hola a todos" << std::endl;
+    User users[MAX_USERS];
+    users[0] = {1, 1234, "Alice", 1000.0};
+
+    std::cout << "---- BANKCLI INFORMATION ----" << std::endl;
+    std::cout << "id: " << users[0].id << std::endl;
+    std::cout << "pin: " << users[0].pin << std::endl;
+    std::cout << "name: " << users[0].name << std::endl;
+    std::cout << "balance: " << users[0].balance << std::endl;
     return 0;
 }
