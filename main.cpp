1b22ca7abfad4b6ad2366c052616c0bc84577095
Luis Rubio <102443174+LMR04@users.noreply.github.com>
2026-04-21 11:57:41 -0500

    feat: main menu defined

--- main.cpp
+++ main.cpp
@@ -12,6 +12,39 @@ struct User {
 
 int main() {
     User users[MAX_USERS];
+    int option;
+    do {
+        std::cout << "---- BANKCLI ----" << std::endl;
+        std::cout << "1. Check balance" << std::endl;
+        std::cout << "2. Deposit" << std::endl;
+        std::cout << "3. Withdraw" << std::endl;
+        std::cout << "4. Transfer" << std::endl;
+        std::cout << "5. Exit" << std::endl;
+        std::cout << "Select an option: ";
+        std::cin >> option;
+        switch (option){
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        default:
+            std::cout << "Invalid option. Please try again." << std::endl;
+            break;
+        }
+    } while (option != 5);
+    
+
     users[0] = {1, 1234, "Alice", 1000.0};
 
     std::cout << "---- BANKCLI INFORMATION ----" << std::endl;
